8d8ff112daf16594b729404319683c822db88060
yatishan <yatishan54@gmail.com>
2025-08-28 16:58:01 +0630

    update data

--- routes/api.php
+++ routes/api.php
@@ -8,12 +8,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/product',[ProductContoller::class,'index']);
-Route::post('/product',[ProductContoller::class,'store']);
-Route::get('/product/{id}',[ProductContoller::class,'show']);
-Route::put('/product/{id}',[ProductContoller::class,'update']);
-Route::delete('/product/{id}',[ProductContoller::class,'destroy']);
+// Route::get('/product',[ProductContoller::class,'index']);
+// Route::post('/product',[ProductContoller::class,'store']);
+// Route::get('/product/{id}',[ProductContoller::class,'show']);
+// Route::put('/product/{id}',[ProductContoller::class,'update']);
+// Route::delete('/product/{id}',[ProductContoller::class,'destroy']);
 
-
-
+Route::apiResource('product',ProductContoller::class);
 
